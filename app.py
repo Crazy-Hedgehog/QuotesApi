12c32c6d63ec06e20b48c8f53dd4df1c38620bc4
student <crazy_hedgehog@mail.ru>
2022-12-01 18:06:11 +0300

    add method POST

--- app.py
+++ app.py
@@ -1,35 +1,36 @@
 from flask import Flask
+from flask import request
 import random
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 about_me = {
-   "name": "Евгений",
-   "surname": "Юрченко",
-   "email": "eyurchenko@specialist.ru"
+   'name': 'Евгений',
+   'surname': 'Юрченко',
+   'email': 'eyurchenko@specialist.ru'
 }
 
 quotes = [
    {
-       "id": 3,
-       "author": "Rick Cook",
-       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+       'id': 3,
+       'author': 'Rick Cook',
+       'text': 'Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает.'
    },
    {
-       "id": 5,
-       "author": "Waldi Ravens",
-       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+       'id': 5,
+       'author': 'Waldi Ravens',
+       'text': 'Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках.'
    },
    {
-       "id": 6,
-       "author": "Mosher’s Law of Software Engineering",
-       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+       'id': 6,
+       'author': 'Mosher’s Law of Software Engineering',
+       'text': 'Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили.'
    },
    {
-       "id": 8,
-       "author": "Yoggi Berra",
-       "text": "В теории, теория и практика неразделимы. На практике это не так."
+       'id': 8,
+       'author': 'Yoggi Berra',
+       'text': 'В теории, теория и практика неразделимы. На практике это не так.'
    },
 
 ]
@@ -41,14 +42,14 @@ def get_quotes():
    return quotes
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
    return about_me
 
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-   return "Hello, World!"
+   return 'Hello, World!'
 
 
 @app.route('/quotes/<int:quote_id>')
@@ -56,22 +57,30 @@ def find_quote(quote_id):
    for quote in quotes:
       if quote['id'] == quote_id:
          return quote
-   return f"Quote with id={quote_id} not found", 404
+   return f'Quote with id={quote_id} not found', 404
 
 
 @app.route('/quotes/count')
 def quotes_counter():
-   counter = {}
-   counter['count'] = len(quotes)
-   return counter
+   return {
+            'count': len(quotes)
+   }
 
-
-@app.route('/quotes/random_quote')
+@app.route('/quotes/random')
 def get_random_quote():
    return random.choice(quotes)
 
 
-if __name__ == "__main__":
+@app.route('/quotes', methods=['POST'])
+def create_quote():
+    data = request.json
+    new_quote = data
+    new_quote['id'] = quotes[-1]['id'] + 1
+    quotes.append(new_quote)
+    return new_quote, 201
+
+
+if __name__ == '__main__':
    app.run(debug=True)
 
 
